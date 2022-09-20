a548d23c1176cf395857952b2f595faeb1127df8
JorgeLuis-RJ <65325610+JorgeLuis-RJ@users.noreply.github.com>
2022-09-19 22:31:16 -0500

    Second Version

--- Rob22S2_Lab2_PoseSetting.m
+++ Rob22S2_Lab2_PoseSetting.m
@@ -1,3 +1,19 @@
 %%
 rosinit; % Conexión con nodo maestro
 %%
+setPoseAbsClient = rossvcclient('/turtle1/teleport_absolute'); % Creación del cliente
+setPoseAbsReq = rosmessage(setPoseAbsClient); % Creación del mensaje
+setPoseAbsReq.X = 5; % Declaración del valor 'X' del servicio
+setPoseAbsReq.Y = 5; % Declaración del valor 'Y' del servicio
+setPoseAbsReq.Theta = 3; % Declaración del valor 'Theta' del servicio
+%%
+setPoseAbsResp = call(setPoseAbsClient, setPoseAbsReq); % Solicitud para realizar el servicio
+%%
+setPoseRelClient = rossvcclient('/turtle1/teleport_relative'); % Creación del cliente
+setPoseRelReq = rosmessage(setPoseRelClient); % Creación del mensaje
+setPoseRelReq.Linear = 1.2; % Declaración del valor 'Linear' del servicio
+setPoseRelReq.Angular = 0; % Declaración del valor 'Angular' del servicio
+%%
+setPoseRelResp = call(setPoseRelClient, setPoseRelReq); % Solicitud para realizar el servicio
+%%
+rosshutdown; % Desconexión del nodo global de Matlab
